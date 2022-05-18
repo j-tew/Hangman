f333060363eeec242dd2f826f85921e7cd9e9773
j-tew <justinrtew@gmail.com>
2022-05-18 14:37:28 -0500

    update Hangman game using OOP concepts

--- hangman.py
+++ hangman.py
@@ -1,249 +1,103 @@
-import random
+import random, assets
 from os import system, name
 
+class Hangman():
+    def __init__(self):
+        # Banner Text
+        self.banner = assets.banner
+        # Menu Text
+        self.menu = assets.menu
+        # Dictionary of Categories
+        self.categories = assets.categories
+        # Dictionary of Scenes
+        self.scenes = assets.scenes
+        # Starting Scene
+        self.scene = 0
+        # Game Word
+        self.word = ''
+        # Game Board
+        self.board = {}
+        # Board Map
+        self.board_map = {}
+        # Guessed Letters
+        self.guessed = []
 
-'''------------------------------------------------------------
-Welcome to my Hangman game!
+    # Clearing the terminal
+    def clear(self) -> None:
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
 
-**EXTRAS** (Future Updates)
-- [ ] Beginning UI
-        - Categories (make sports a submenu?)
-------------------------------------------------------------'''
+    # Selecting a random word
+    def select_word(self):
+        '''Select a random word from a text file based on category selection'''
+        self.clear()
+        selection = 0
+        # Validate user input for category selection
+        while not selection:
+            print(self.banner)
+            try:
+                selection = int(input(self.menu))
+                # Set the word from the text files
+                with open(self.categories[selection], "r") as wordlist:
+                    self.word = random.choice(wordlist.readlines())
+                    self.board = {index: letter for index, letter in enumerate(self.word)}
+                    self.board_map = {index: not letter.isalpha() for index, letter in self.board.items()}
+            # Handle invalid selections
+            except KeyError:
+                self.clear()
+                print('Please enter a valid selection.')
+                selection = 0
+            # Handle strings
+            except ValueError:
+                self.clear()
+                print('Please enter a number.')
+
+    # Displaying a Game Board
+    def display_board(self):
+        '''Use map and board to display hidden letters'''
+        hidden_letters = [self.board[i] if self.board_map[i] else '_' for i in self.board]
+        print(*hidden_letters)
+
+    # Updating the Board Map
+    def update_map(self, guess: str):
+        '''Update the map to track correct guesses'''
+        for index, letter in self.board.items():
+            if letter == guess:
+                self.board_map[index] = True
+    # Game Loop
+    def play(self):
+        '''Play the game!'''
+        self.select_word()
+        self.clear()
+        while False in self.board_map.values() and self.scene < 6:
+            # User Interface
+            print(self.banner)
+            print(self.scenes[self.scene])
+            self.display_board()
+            letter = input('Guess a letter: ').upper()
+            # No penalty for guessing letters that have 
+            if letter in self.guessed:
+                self.clear()
+                print('That letter has already been guessed!')
+            # Correct guess, update game board
+            elif letter in self.word:
+                self.clear()
+                self.guessed.append(letter)
+                self.update_map(letter)
+            # Wrong guess, update scene
+            else:
+                self.clear()
+                self.guessed.append(letter)
+                self.scene += 1
+        # End Game
+        print(self.banner)
+        print(self.scenes[self.scene])
+        print('GAME OVER!')
 
 
-# Banner
-banner = '''
- _   _
-| | | | __ _ _ __   __ _ _ __ ___   __ _ _ __
-| |_| |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-|  _  | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                   |___/
-'''
+if __name__ == '__main__':
+    game = Hangman()
+    game.play()
 
-# Dictionary for the scenes
-scene = {
-    0: '''
-         ___________ 
-        |           |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |''',
-
-    1: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |
-        |''',
-
-    2: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |           |
-        |           |
-        |
-        |
-        |
-        |
-        |
-        |
-        |''',
-
-    3: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |         \_|
-        |           |
-        |
-        |
-        |
-        |
-        |
-        |
-        |''',
-
-    4: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |         \_|_/
-        |           |
-        |
-        |
-        |
-        |
-        |
-        |
-        |''',
-
-    5: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |         \_|_/
-        |           |
-        |          /
-        |         /
-        |
-        |
-        |
-        |
-        |''',
-
-    6: '''
-         ___________ 
-        |           |
-        |          ,,,
-        |         (X_X)
-        |         \_|_/
-        |           |
-        |          / \\
-        |         /   \\
-        |
-        |
-        |
-        |
-        |'''
-}
-
-menu = '''
-1) Baseball Teams
-2) Country Artists
-3) Disney Movies
-
-'''
-
-# Dictionary of categories
-categories = {
-    1: "baseball_teams.txt",
-    2: "country_artists.txt",
-    3: "disney.txt"
-}
-
-# Credit to ChickenParm for a function to make the game not junk up the terminal
-def clear():
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
-
-# Selecting a random word.
-def wordGen(sel, catDict):
-    wordlist = open(catDict[sel], "r")
-    words = wordlist.readlines()
-    word = random.choice(words)
-    wordlist.close()
-    return word
-# testWord = "devildog"
-
-# Updating the game board with guesses
-def updateBoard(brd, wrd, ltr):
-    places = []
-    index = 0
-    for i in wrd:
-        if i == ltr:
-            places.append(index)
-        index += 1
-    for i in places:
-        brd[i] = ltr
-    return brd
-
-def updateWord(wrd, ltr):
-    trans = wrd.maketrans(ltr, "-", "\n")
-    wrd = wrd.translate(trans)
-    return wrd
-
-# Accepting guesses
-def getGuess():
-    guess = input("Please Enter A Letter: ")
-    return guess.upper()
-
-# Generating a board of underscores to display
-def boardGen(wrd):
-    letters = list(wrd)
-    letters.remove(letters[-1])
-    board = []
-    for i in letters:
-        if i.isalpha():
-            board.append("_")
-        elif i == " ":
-            board.append(" ")
-    return board
-
-# Generate the Phrase
-
-clear()
-print(banner)
-selection = input(f"Please Select a Category:\n{menu}")        
-word = wordGen(int(selection), categories)
-board = boardGen(word)
-
-# Make it Loop
-key = 0
-
-### TESTING ###
-# print(word)
-# print(*board)
-# letter = getGuess()
-# update(board, word, letter)
-# print(word)
-# print(*board)
-
-# Game Loop
-while word.count("-") < len(word):
-    # Keeping the game board clean
-    clear()
-    print(banner)
-    # Hangman scenes
-    print(scene[key])
-    # Word to be guessed
-    print(f"Secret Word: ", *board, "\n")
-    # If dude is complete or if the word is complete break the loop
-    if key == 6 or "_" not in board:
-        break
-    # Take guesses as input
-    letter = getGuess()
-    # Checking guesses and updating the scene and board accordingly
-    if len(letter) > 1 or len(letter) < 1:
-        key += 1
-    elif letter == " ":
-        key += 1
-    elif letter in word:
-        board = updateBoard(board, word, letter)
-        word = updateWord(word, letter)
-    else:
-        key += 1
-# Ending the Game
-if "_" not in board:
-    clear()
-    print(banner)
-    print(scene[key])
-    print(f"Secret Word: ", *board)
-    print("\tYOU WIN!!!")
-else:
-    clear()
-    print(banner)
-    print(scene[6], "GAME OVER!!!")
